f42c140d3d0b9ff96bac7f9f9c6aaf1d52bcc7d6
Jingwei Ye <yjjw97@gmail.com>
2021-12-05 14:44:52 -0500

    test

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,8 @@
 public class Main {
 
     public static void main( String[] args ) {
-        System.out.println( "final project" );
+        System.out.println( "this is a test" );
         String filePath = "";
         TransactionManager tm = new TransactionManager();
-
     }
 }
