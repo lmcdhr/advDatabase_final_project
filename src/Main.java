40d7f47dff67f427815de4bfb97da6e8dc534c5d
lmcdhr <luomucheng2018@outlook.com>
2021-12-06 10:30:04 -0500

    bug fixed for read and RO

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main( String[] args ) {
         System.out.println( "this is a test" );
-        String filePath = "/Users/muchengluo/Desktop/NYU/2434_adv_database/project/src/tests/test1.txt";
+        String filePath = "src/tests/test1.txt";
         TransactionManager tm = new TransactionManager();
         tm.readInputFile(filePath);
     }
