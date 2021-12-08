aba43dda89aadd03227b07828da3f0f1066f495d
Jingwei Ye <yjjw97@gmail.com>
2021-12-07 23:16:58 -0500

    args: read input and write output

--- src/Main.java
+++ src/Main.java
@@ -4,17 +4,32 @@
 Date: 12/07/2021*/
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
 
 public class Main {
 
     public static void main( String[] args ) {
-        File directoryPath = new File("src/tests");
+        if (args.length != 2){
+            System.out.println("expected 2 arguments");
+        }
+        String inputfiles = args[0];
+        String outputfiles = args[1];
+
+        File inDirectoryPath = new File(inputfiles);
         TransactionManager tm = new TransactionManager();
-        String[] fileNames = directoryPath.list();
+        String[] fileNames = inDirectoryPath.list();
         String newLine = System.getProperty("line.separator");
         for(int i=0; i<fileNames.length; i++){
-            System.out.println(newLine + "This is test " + i);
-            tm.readInputFile("src/tests/" + fileNames[i]);
+            File output = new File(outputfiles + "/output" + i + ".txt");
+            try {
+                PrintStream stream = new PrintStream(output);
+                System.out.println(newLine + "This is test " + i);
+                tm.readInputFile("inputfiles/" + fileNames[i]);
+                System.setOut(stream);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
         }
         // tm.dm.dump();
     }
