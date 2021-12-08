cb982476789b0cf3dab2efbaf9c51cdd27f410b5
Jingwei Ye <yjjw97@gmail.com>
2021-12-07 19:48:50 -0500

    read all text inputs in a directory

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # advDatabase_final_project
+Team Members:
+1. Mucheng Luo (ml6389)
+2. Jingwei Ye (jy3555)
+
+All test input files are put under the directory path "src/tests"
+Our program will read each file in this directory.
--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,19 @@
 2. Jingwei Ye (jy3555)
 Date: 12/07/2021*/
 
+import java.io.File;
+
 public class Main {
 
     public static void main( String[] args ) {
-        String filePath = "src/tests/test22.txt";
+        File directoryPath = new File("src/tests");
         TransactionManager tm = new TransactionManager();
-        tm.readInputFile(filePath);
-        //tm.dm.dump();
+        String[] fileNames = directoryPath.list();
+        String newLine = System.getProperty("line.separator");
+        for(int i=0; i<fileNames.length; i++){
+            System.out.println(newLine + "This is test " + i);
+            tm.readInputFile("src/tests/" + fileNames[i]);
+        }
+        // tm.dm.dump();
     }
 }
